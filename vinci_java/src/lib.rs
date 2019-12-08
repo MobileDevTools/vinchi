543f9844d7940ffc1ce863ff5f65bd7a60e846e7
vaiklol <samokatanet@gmail.com>
2019-12-08 18:10:53 +0300

    change java methods, add files walk (in progress)

--- vinci_java/src/lib.rs
+++ vinci_java/src/lib.rs
@@ -1,24 +1,24 @@
-use vinci_core::CompileError;
+use vinci_core::{CompileError, StreamExt, async_await::FusedFuture, async_walk_and_find_all_files};
 use std::process::{Command, Stdio};
 use std::io::{Read, Stdout, Write};
-use std::borrow::{Cow, Borrow};
-use std::ffi::OsStr;
 
 // GLOBAL TODO make in with trait and in diff mods, and all same things should be in the CORE!!!!
 
-const JAVA_PACKAGES: [&'static str; 7] = [
-    "/usr/bin/javac",
-    "/usr/bin/jar",
-    "/usr/bin/java",
-    "/usr/bin/jarsigner",
-    "/usr/bin/javadoc",
-    "/usr/bin/javah",
-    /*not so important (exists since jdk 1.6) */"/usr/bin/javapackager"]
-;
+const USER_BIN: &str = "/usr/bin/";
+const JAVA_HOME: &str = "JAVA_HOME";
+const JAVA_PACKAGES: [&'static str; 6] = [
+    "javac",
+    "jar",
+    "java",
+    "jarsigner",
+    "javadoc",
+    "javah", ];
 
-async fn compile() -> Result<usize, CompileError> {
+///*not so important (exists since jdk 1.6) */"/usr/bin/javapackager"
+
+async fn compile(r#in: String) -> Result<usize, CompileError> {
     // find java first action
-    let is_installed = is_java_installed().await;
+    let is_installed = get_java_installed_version().await;
 
     if is_installed.is_err() {
         return Err(CompileError::new(
@@ -26,73 +26,81 @@ async fn compile() -> Result<usize, CompileError> {
             "yours files".into(),
             "Java missing, want to install it? [Y/n]".into()));
     }
+    // set java home
+    // find by which java!!
+    let java_path = if let Ok(java_home) = check_java_home().await {
+        java_home
+    } else {
+        USER_BIN.to_owned()
+    };
     // check packages
-
-    let all_packages_exists = is_all_java_packages_exists();
+    let all_packages_exists = is_all_java_packages_exists(&java_path).await;
+    // find all files in target dir
+    let java_files = async_walk_and_find_all_files(&r#in).await;
 
     Ok(0)
 }
 
-async fn is_java_installed() -> Result<String, ()> {
-    #[cfg(target_os = "macos")]
-        let all_java_versions = check_and_return_out(
-        Command::new("/usr/libexec/java_home")
-            .arg(" -V"));
+async fn check_java_home() -> Result<String, ()> {
+    std::env::var(JAVA_HOME)
+        .map_err(|e| eprintln!("{}", e))
+        .and_then(|home| if home.is_empty() { Err(()) } else { Ok(home) })
+}
 
-    #[cfg(target_os = "linux")]
-        let all_java_versions = check_and_return_out(
-        Command::new("/usr/bin/update-alternatives")
-            .arg(" --config java"));
+async fn get_java_installed_version() -> Result<String, ()> {
 
     //todo make request from config for concrete jdk version
     // here
-    check_and_return_out(Command::new("/usr/bin/java")
-        .arg("--version"))
-}
+//    #[cfg(target_os = "macos")]
+//        let all_java_versions = check_and_return_out(
+//        Command::new("/usr/libexec/java_home")
+//            .arg(" -V"));
+//
+//    #[cfg(target_os = "linux")]
+//        let all_java_versions = check_and_return_out(
+//        Command::new("/usr/bin/update-alternatives")
+//            .arg(" --config java"));
 
-#[cfg(target_os = "linux")]
-fn check_and_return_out(out: &mut Command) -> Result<String, ()> {
-    out.spawn()
-        .map_err(|e| eprintln!("{}", e))
-        .and_then(|child| {
-            let std_in = child.stdin
-                .map(|mut std_in| std_in.write_all(b"\n"));
-            let out_string = child.stdout
-                .map(|child_out|
-                    child_out.bytes().collect::<Vec<_>>()).unwrap_or(Default::default());
-            String::from_utf8(out_string.into_iter().map(|b| b.unwrap_or(0)).collect())
-                .map_err(|e| eprintln!("{}", e))
+
+    check_and_return_err_out(Command::new("java")
+        .arg("-version"))
+        .map(|java| {
+            println!("Found {}", java);
+            Ok(java)
         })
+        .unwrap_or(Err(()))
 }
 
-#[cfg(target_os = "macos")]
-fn check_and_return_out(out: &mut Command) -> Result<String, ()> {
+fn check_and_return_err_out(out: &mut Command) -> Result<String, ()> {
     out.output()
         .map_err(|e| eprintln!("{}", e))
         .and_then(|child|
-            String::from_utf8(child.stdout)
+            String::from_utf8(child.stderr)
                 .map_err(|e| eprintln!("{}", e))
         )
 }
 
-async fn is_all_java_packages_exists() -> bool {
+async fn is_all_java_packages_exists(java_path: &str) -> bool {
     let files = JAVA_PACKAGES.iter()
-        .map(|file| {
-            async_std::path::Path::new(file).exists()
-        })
+        .map(|java_bin| format!("{}{}", java_path, java_bin))
         .collect::<Vec<_>>();
-    vinci_core::future::join_all(files).await.iter().all(|is| *is)
+    let mut exists = vec![];
+    for file in files.iter() {
+        let package_path = std::path::Path::new(file).exists();
+        exists.push(package_path);
+    }
+    exists.into_iter().all(|is| is)
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{compile, is_java_installed, is_all_java_packages_exists};
+    use crate::{compile, get_java_installed_version, is_all_java_packages_exists, JAVA_HOME, USER_BIN, async_walk_and_find_all_files};
 
     #[test]
     fn should_check_java_installed() {
         //haven ideas how write tests // todo about it
-        async_std::task::block_on(async {
-            let result = is_java_installed().await;
+        vinci_core::block_on(async {
+            let result = get_java_installed_version().await;
             println!("{:?}", result);
         });
     }
@@ -100,8 +108,8 @@ mod test {
     #[test]
     fn should_check_all_java_packages_is_installed() {
         //haven ideas how write tests // todo about it
-        async_std::task::block_on(async {
-            let result = is_all_java_packages_exists().await;
+        vinci_core::block_on(async {
+            let result = is_all_java_packages_exists(USER_BIN).await;
             println!("{:?}", result);
         });
     }
